34ccfc76eba9272f7ab6ab479cfb03de7e701475
cvine21 <93055613+cvine21@users.noreply.github.com>
2022-02-27 13:24:22 +0300

    finished

---
+++ backup_ver4.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import zipfile
+
+# 1. The files and directories to be backed up are
+# specified in a list.
+source = [
+	'/mnt/c/Users/Руфина/PycharmProjects/problem_solving',
+	'/mnt/c/Users/Руфина/PycharmProjects/python-test']
+
+# 2. The backup must be stored in a
+# main backup directory
+target_dir = '/mnt/c/Users/Руфина/backup'
+
+# 3. The files are backed up into a zip file.
+# 4. The current day is the name of the subdirectory
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+# The current time is the name of the zip archive.
+now = time.strftime('%H%M%S')
+
+# initializing empty file paths list
+file_paths = []
+# crawling through directory and subdirectories
+for directory in source:
+	for root, subdirectories, files in os.walk(directory):
+		for filename in files:
+			# join the two strings in order to form the full filepath.
+			filepath = os.path.join(root, filename)
+			file_paths.append(filepath)
+
+# Take a comment from the user to
+# create the name of the zip file
+comment = input('Enter a comment --> ')
+
+# Check if a comment was entered
+if len(comment) == 0:
+	target = today + os.sep + now + '.zip'
+else:
+	target = today + os.sep + now + '_' + \
+		comment.replace(" ", "_") + '.zip'
+
+# Create target directory if it is not present
+if not os.path.exists(today):
+	os.mkdir(today)
+	print('Successfully created directory', today)
+
+# 5. Writing files to a zipfile
+print('Running:')
+with zipfile.ZipFile(target, 'w') as zip:
+	for file in file_paths:
+		print('  adding:', file)
+		zip.write(file)
+
+print('Successful backup to', target)
